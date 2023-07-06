7545e38de9dae9a20289134e04f89ad5fe7da19d
soulwinter <1127907836@qq.com>
2023-07-06 14:27:16 +0800

    Initial Commit

---
+++ PosePerfect/PosePerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosePerfectApp.swift
+//  PosePerfect
+//
+//  Created by Han Chubo on 2023/7/6.
+//
+
+import SwiftUI
+
+@main
+struct PosePerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
